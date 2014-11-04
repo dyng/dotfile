d2753c88502e39d02b949c099d918840dc1cf2f7
dyng <dygvirus@gmail.com>
2014-11-04 21:34:15 +0900

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+deploy () {
+    wd=$1
+    # config files start with '_'
+    file_list=$(find $1 -depth 1 -regex "$1/_.*")
+    for file in $file_list; do
+        target_file=/tmp/$(basename $file|sed -E 's/^_/./')
+        if [[ -e $target_file ]]; then
+            # if target exists and is symlink
+            if [[ -h $target_file ]]; then
+                rm -f $target_file
+            else
+                echo "$target_file exists but is not a symlink! Ignore it."
+                break
+            fi
+        fi
+        ln -s $PWD/$file $target_file
+    done
+    echo "deploying $wd done."
+}
+
+paks=(git zsh vim tmux bash vimperator)
+
+for pak in ${paks[@]}; do
+    deploy $pak
+done
