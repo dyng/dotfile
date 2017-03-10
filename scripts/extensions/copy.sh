26dc60b6aed15004d72c46e61f8b7ebd13efe592
dyng <dygvirus@gmail.com>
2017-03-10 16:03:21 +0800

    sh: add copypath()

--- scripts/extensions/copy.sh
+++ scripts/extensions/copy.sh
@@ -1,9 +1,35 @@
 copy() {
     if [[ `uname` = 'Darwin' ]]; then
-        if `which reattach-to-user-namespace &>/dev/null`; then
-            cat $1 |reattach-to-user-namespace pbcopy
+        if [[ ! -n $TMUX ]]; then
+            if `which reattach-to-user-namespace &>/dev/null`; then
+                cat $1 |reattach-to-user-namespace pbcopy
+            else
+                echo "reattach-to-user-namespace is required while copy under tmux environment!"
+                echo "use homebrew to install: brew install reattach-to-user-namespace"
+            fi
         else
-            echo "reattach-to-user-namespace is required while copy under tmux environment!"
+            cat $1 |pbcopy
+        fi
+    else
+        echo 'Linux support need to be implemented.'
+    fi
+}
+
+copypath() {
+    if [[ `uname` = 'Darwin' ]]; then
+        if ! `which greadlink &>/dev/null`; then
+            echo 'use homebrew to install: brew install coreutils'
+        fi
+
+        if [[ ! -n $TMUX ]]; then
+            if `which reattach-to-user-namespace &>/dev/null`; then
+                greadlink -f $1 |tr -d '\r\n' |reattach-to-user-namespace pbcopy
+            else
+                echo "reattach-to-user-namespace is required while copy under tmux environment!"
+                echo "use homebrew to install: brew install reattach-to-user-namespace"
+            fi
+        else
+            greadlink -f $1 |tr -d '\r\n' |pbcopy
         fi
     else
         echo 'Linux support need to be implemented.'
