292416157613085811c4db18fc1148021369a05f
dyng <dygvirus@gmail.com>
2018-03-26 11:05:32 +0800

    osx: change default KeyRepeat

--- osx/setting.sh
+++ osx/setting.sh
@@ -3,3 +3,10 @@ if [[ ! -e $HOME/Screenshots ]]; then
     mkdir $HOME/Screenshots
 fi
 defaults write com.apple.screencapture location $HOME/Screenshots/
+
+# setting key repeat
+# normal minimum is 15 (225 ms)
+defaults write -g InitialKeyRepeat -int 10 # 150ms
+
+# normal minimum is 2 (30 ms)
+defaults write -g KeyRepeat -int 1 # 15ms
