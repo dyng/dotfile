242bba6db995baaedc4f0616c29e315280ed3662
dyng <dygvirus@gmail.com>
2018-06-22 13:54:42 +0800

    shell: improve scripts

--- scripts/extensions/mvln.sh
+++ scripts/extensions/mvln.sh
@@ -1,24 +1,22 @@
 mvln () {
-    if [[ -e $2 ]]; then
-        if [[ -d $2 ]]; then
-            filename=$(basename $1)
-            dest=$2/$filename
-        else
-            dest=$2
+    while [[ $# > 1 ]]; do
+        src=$1
+        dest=${@:$#}
+        if [[ -e $dest && -d $dest ]]; then
+            dest=$dest/$(basename $1)
         fi
-    else
-        dest=$2
-    fi
-    src=$1
 
-    if [[ "${dest:0:1}" = "/" ]]; then
-        abs_dest=$dest
-    else
-        abs_dest=$PWD/$dest
-    fi
+        if [[ "${dest:0:1}" = "/" ]]; then
+            abs_dest=$dest
+        else
+            abs_dest=$PWD/$dest
+        fi
 
-    echo "moving $src to $dest"
-    mv $src $dest
-    echo "linking $dest to $src"
-    ln -s $abs_dest $src
+        echo "moving $src to $dest"
+        mv $src $dest
+        echo "linking $dest to $src"
+        ln -s $abs_dest $src
+
+        shift
+    done
 }
