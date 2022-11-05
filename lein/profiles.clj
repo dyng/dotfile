bfd0ad3f8ccd7cc52c35ce38f22c50a6bdd6a1d3
dyng <dygvirus@gmail.com>
2022-11-05 13:35:44 +0800

    remove unnecessary files

--- lein/profiles.clj
+++
@@ -1,5 +0,0 @@
-{:user {:plugins [;[cider/cider-nrepl "0.9.1"]
-                  ]}
- :repl {:dependencies [[org.clojure/clojure "1.7.0"]
-                       [alembic "0.2.0"]
-                       ]}}
